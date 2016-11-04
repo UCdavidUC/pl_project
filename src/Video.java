4f80e38b7819b8152589cf2f260db331d0edb35a
Alejandra Martínez <alemartinez092@gmail.com>
2016-11-04 11:34:36 -0600

    Main and video class

---
+++ src/Video.java
@@ -0,0 +1,11 @@
+public class Video extends Media {
+	public Video () {}
+
+	protected loadVideoFile(String videoName) {
+		
+	}
+
+	protected display(File file, int display) {
+		System.out.println()
+	}
+}
